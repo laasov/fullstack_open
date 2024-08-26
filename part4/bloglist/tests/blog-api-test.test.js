4260935193b4ca36e7ead384a7efeee2278bffe8
laasov <lassi.o.virtanen@gmail.com>
2024-08-26 23:30:30 +0300

    4.9. and 4.10.

--- part4/bloglist/tests/blog-api-test.test.js
+++ part4/bloglist/tests/blog-api-test.test.js
@@ -3,20 +3,29 @@ const Blog = require('../models/blog')
 const assert = require('node:assert')
 
 const initialBlogs = [
-  { title: "Spaten",
+  { 
+    _id: '5a422aa71b54a676234d17f8',
+    title: "Spaten",
     author: "Lassi",
     url: "www.spatenfakeurl.nam",
     likes: 1000,
+    __v: 0
   },
-  { title: "Paulaner",
+  { 
+    _id: '5a422aa71b54a676234d17f7',
+    title: "Paulaner",
     author: "Lassi",
     url: "www.whaaaaat.wtf",
     likes: 100,
+    __v: 0
   },
-  { title: "Wasser",
+  { 
+    _id: '5a422aa71b54a676234d17f6',
+    title: "Wasser",
     author: "BigBang",
     url: "www.vanhinvoitehista.yes",
     likes: 100,
+    __v: 0
   },
 ]
 
@@ -49,6 +58,40 @@ test('blogs are returned as json', async () => {
 test('there are exactly three blog posts', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+// 4.9.
+test('blog posts are identified by the property "id"', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+  const res = blogs.map( blog => Object.hasOwn(blog, 'id'))
+
+  assert.strictEqual(true, res.every(val => val === true))
+})
+
+// 4.10
+test('HTTP POST to /api/blogs increases blog count by one"', async () => {
+  let response = await api.get('/api/blogs')
+  const initialCount = response.body.length
+
+  const newBlog = Blog ({
+    _id: '5a422aa71b54a676234d17f6',
+    title: 'Test Blog',
+    author: 'Hammurabi',
+    url: 'www.gochujang.hot',
+    likes: 100000000,
+    _v: 0
+  })
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  response = await api.get('/api/blogs')
+  const newCount = response.body.length
+
+  assert.strictEqual(newCount, initialCount + 1)
 })
 
 after(async () => {
