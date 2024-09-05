158e534af808d9389c055fc9844d558a568649bf
laasov <lassi.o.virtanen@gmail.com>
2024-09-05 18:05:04 +0300

    4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogRouteController')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
